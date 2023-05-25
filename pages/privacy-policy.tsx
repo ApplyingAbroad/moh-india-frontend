66dce1c15108914b6fbde264e9576caa8ac41cfe
Gautam Singla <ggsingla777@gmail.com>
2023-05-25 14:20:36 +0530

    Terms of service page added

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -8,15 +8,15 @@ const Privacypolicy = () => {
     <main className='bg-beige-50'>
       <NavBar />
       <LogoBar />
-      <div className='text-center my-6'>
+      <div className='text-center my-6 prose mx-auto px-4 lg:px-0'>
         <p className='uppercase text-sm'>Privacy first, always</p>
 
         <h1 className='font-serif text-5xl font-bold'>Privacy Policy</h1>
       </div>
       <div className='my-16 prose prose-indigo mx-auto px-6 lg:px-0'>
-        <div className='container space-y-2 mb-6'>
+        <div className='space-y-2 mb-6'>
           <h2 className='font-bold text-xl'>Introduction</h2>
-          <p className='   text-my-gray3 text-base  '>
+          <p className='text-stone-500 text-base'>
             At tailorandcompany.com , we are committed to protecting the privacy
             of our users. This privacy policy explains how we collect, use, and
             protect the personal information of our users. By using our website
@@ -25,10 +25,10 @@ const Privacypolicy = () => {
           </p>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>Definitions</h2>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>Definitions</h2>
 
-          <ul className='  text-my-gray3 text-base   '>
+          <ul className='text-stone-500 text-base'>
             <li>
               <strong>Data</strong> refers to any information that can be used
               to identify an individual, including but not limited to names,
@@ -49,13 +49,12 @@ const Privacypolicy = () => {
           </ul>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>Types of Data Collected</h2>
-          <p className='   text-my-gray3 text-base  '>
-            {' '}
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>Types of Data Collected</h2>
+          <p className='text-stone-500 text-base'>
             We collect the following types of data from our users:
           </p>
-          <ul className='  text-my-gray3 text-base   '>
+          <ul className='text-stone-500 text-base'>
             <li>
               Personal data. This includes data such as names, addresses, email
               addresses, and other contact information that users provide when
@@ -72,7 +71,7 @@ const Privacypolicy = () => {
               access our website or services.
             </li>
 
-            <p className='   text-my-gray3 text-base  '>
+            <p className='text-stone-500 text-base'>
               We may also collect personal data from third-party sources, such
               as social media platforms, to enhance our services and provide
               more personalized experiences for our users.
@@ -80,9 +79,9 @@ const Privacypolicy = () => {
           </ul>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>CalOPPA</h2>
-          <p className='   text-my-gray3 text-base  '>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>CalOPPA</h2>
+          <p className='text-stone-500 text-base'>
             We comply with the California Online Privacy Protection Act
             (CalOPPA), which requires us to post a clear and conspicuous privacy
             policy on our website. This privacy policy is intended to inform
@@ -92,9 +91,9 @@ const Privacypolicy = () => {
           </p>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>GDPR</h2>
-          <p className='   text-my-gray3 text-base  '>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>GDPR</h2>
+          <p className='text-stone-500 text-base'>
             We are also committed to complying with the General Data Protection
             Regulation (GDPR), which applies to the processing of personal data
             of individuals in the European Union. This means that we will only
@@ -104,13 +103,13 @@ const Privacypolicy = () => {
           </p>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>Data Sharing</h2>
-          <p className='   text-my-gray3 text-base  '>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>Data Sharing</h2>
+          <p className='text-stone-500 text-base'>
             We may share your personal information with third parties for the
             following purposes:
           </p>
-          <ul className='  text-my-gray3 text-base   '>
+          <ul className='text-stone-500 text-base'>
             <li>
               To provide the services you have requested. For example, if you
               sign up for a trial of our product, we will share your information
@@ -131,14 +130,14 @@ const Privacypolicy = () => {
           </ul>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>Data Protection</h2>
-          <p className='   text-my-gray3 text-base  '>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>Data Protection</h2>
+          <p className='text-stone-500 text-base'>
             We take reasonable steps to protect your personal information from
             unauthorized access, use, or disclosure. Some of the measures we use
             to protect your information include:
           </p>
-          <ul className='  text-my-gray3 text-base   '>
+          <ul className='text-stone-500 text-base'>
             <li>
               Encrypting personal information in transit and at rest using
               industry-standard protocols.
@@ -154,15 +153,15 @@ const Privacypolicy = () => {
           </ul>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>Changes to this Policy</h2>
-          <p className='   text-my-gray3 text-base  '>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>Changes to this Policy</h2>
+          <p className='text-stone-500 text-base'>
             We may update this privacy policy from time to time to reflect
             changes to our practices or for other operational, legal, or
             regulatory reasons. We will post any changes on this page and
             encourage you to review the policy periodically for updates:.
           </p>
-          <ul className='  text-my-gray3 text-base   '>
+          <ul className='text-stone-500 text-base'>
             <li>
               Encrypting personal information in transit and at rest using
               industry-standard protocols.
@@ -178,9 +177,9 @@ const Privacypolicy = () => {
           </ul>
         </div>
 
-        <div className='container space-y-2 mb-6'>
-          <h2 className='font-bold text-xl '>Contact Us</h2>
-          <p className='text-my-gray3 text-base'>
+        <div className='space-y-2 mb-6'>
+          <h2 className='font-bold text-xl'>Contact Us</h2>
+          <p className='text-stone-500 text-base'>
             If you have any questions or concerns about our privacy policy or
             the way we handle your personal information, please contact us at
             hey@tailorandcompany.com .
